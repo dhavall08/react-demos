7e1a2f0c8ee2e973b9cac0d0441d6243f3709cd0
Dhaval <dhaval.laiya@bacancytechnology.com>
2019-01-07 01:27:27 +0530

    React Sample Project first commit

---
+++ demo_project/src/components/Body/Body.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import Forms from './login_forms';
+
+class Body extends Component {
+    render() {
+        return (
+            <div className="body">
+                <div>
+                    <Forms/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Body;
