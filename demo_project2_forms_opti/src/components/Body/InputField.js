6ec5067217bed199ed5cf291586069322c8251a1
Dhaval <dhaval.laiya@bacancytechnology.com>
2019-01-08 17:35:51 +0530

    Removed Component field in import

--- demo_project2_forms_opti/src/components/Body/InputField.js
+++ demo_project2_forms_opti/src/components/Body/InputField.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class InputField extends Component{
-
-    render(){
+const InputField = (props) => {
     return (
         <div>
-        <input className="form" autoComplete="off" id={this.props.id} type={this.props.type} style={{ background: 'white', borderWidth: '2px', border: 'ff4b4b' }} placeholder={this.props.place} onBlur={e => this.props.blurevent(e.target.value,this.props.id)} value={this.props.value} required />
+            <input className="form" autoComplete="off" id={props.id} type={props.type} style={{ background: 'white', borderWidth: '2px', border: 'ff4b4b' }} placeholder={props.place} onBlur={e => props.blurevent(e.target.value, props.id)} value={props.value} required />
         </div>
-        );
-    }
+    );
 }
 
 export default InputField;
