75356d5a006e3573b9d8784ba7bfb5d92928cce9
Dhaval <dhaval.laiya@bacancytechnology.com>
2019-01-21 18:20:02 +0530

    Project Nested Routing with fetching Get,Post,Put

--- project-nested-routing/src/containers/UserInfo/Persons/ComputerDept.js
+++ project-nested-routing/src/containers/UserInfo/Persons/ComputerDept.js
@@ -11,8 +11,8 @@ class ComputerDept extends Component {
 
     getUserData() {
         let login = {
-            email: "sydney@fife",
-            password: ''
+            "email": "peter@klaven",
+            "password": "cityslicka"
         };
 
         this.setState({ loading: true });
@@ -26,20 +26,39 @@ class ComputerDept extends Component {
                 this.setState({ userList: res.data || [], loading: false })
             });
 
-        fetch('https://reqres.in/api/register', {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            mode: "cors", // no-cors, cors, *same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-                "Content-Type": "application/json",
-                // "Content-Type": "application/x-www-form-urlencoded",
-            },
-            redirect: "follow", // manual, *follow, error
-            referrer: "no-referrer", // no-referrer, *client
-            body: JSON.stringify(login), // body data type must match "Content-Type" header
-        }).then(res => res.json()).then(res => console.log(JSON.stringify(login))); //not working
 
+        fetch('https://reqres.in/api/login', {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            body: JSON.stringify(login),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"//for url encoded if pass through encoding
+            } // body data type must match "Content-Type" header
+        }).then(res => res.json()).then(res => console.log("POST ", res)); //not working
+
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PUT',
+            body: JSON.stringify({
+                id: 1,
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => response.json())
+            .then(json => console.log('PUT', json))
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({ tittle: 'asdf', body: 'asdff' })
+        }).then((res) => res.json())
+            .then((data) => console.log('Post ', data))
+            .catch((err) => console.log('Post ', err))
+
+        fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'DELETE'
+        }).then(res=>res.json()).then(res=>console.log('Delete',res));
     }
     render() {
         return (
@@ -58,7 +77,7 @@ class ComputerDept extends Component {
                         </div>
                     })
                 }
-                <img src={this.state.image}/>
+                <img src={this.state.image} />
                 <label>{this.state.name}</label>
             </div>
         );
