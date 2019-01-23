d8f28dd9dc9b0059ecbd14be937b2afa6918f6f2
Dhaval <dhaval.laiya@bacancytechnology.com>
2019-01-23 12:31:36 +0530

    Demo Project

---
+++ 1.demo_project/src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="logo logo2">
+                    <a href="">ReactDemo</a>
+                </div>
+                <ul className="bottom-menu-list">
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Quality Assurance</a></li>
+                    <li><a href="">Brochure</a></li>
+                    <li><a href="">Sitemap</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Footer;
