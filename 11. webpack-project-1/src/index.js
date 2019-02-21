53a8bf5fe9a9f7181431b649a06f9226552b38ce
Dhaval <dhaval.laiya@bacancytechnology.com>
2019-02-21 11:31:53 +0530

    Only webpack configuration

---
+++ 11. webpack-project-1/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const title = 'React Webpack';
+
+ReactDOM.render(
+  <h1>{title}</h1>,
+  document.getElementById('root'),
+);
